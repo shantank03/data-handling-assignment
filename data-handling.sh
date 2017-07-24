4db39fba796c9e9c3c32c65b95864db548998b00
shantank <shantank@live.unc.edu>
2017-07-24 12:08:34 -0400

    edited script

--- data-handling.sh
+++ data-handling.sh
@@ -2,15 +2,19 @@
 
 # Question 1
 echo "What is your area of study?"
-
+read MAJOR
 # Question 2
 echo "Where are you from?"
-
+read ORIGIN
 # Question 3
 echo "What is your favorite color?"
-
+read COLOR
 # Question 4
 echo "What is your favorite food?"
-
+read FOOD
 # Question 5
-echo ""
+echo "From 1-5, how do you rate pizza?"
+read RATING
+
+echo "$MAJOR, $ORIGIN, $COLOR, $FOOD, $RATING"
+paste -d, -s response.list
