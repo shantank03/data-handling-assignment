1617bd4851d6fb9f852f1e69ae3b3d080970ecc3
shantank <shantank@live.unc.edu>
2017-07-24 11:49:23 -0400

    added some files

---
+++ data-handling.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Question 1
+echo "What is your area of study?"
+
+# Question 2
+echo "Where are you from?"
+
+# Question 3
+echo "What is your favorite color?"
+
+# Question 4
+echo "What is your favorite food?"
+
+# Question 5
+echo ""
