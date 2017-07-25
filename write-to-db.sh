829eed92cc5a6bf9e4d9cd2095dd1cd523bce3e0
shantank <shantank@live.unc.edu>
2017-07-24 22:19:25 -0400

    edited stuff

--- write-to-db.sh
+++ write-to-db.sh
@@ -9,10 +9,11 @@ mkdir $TEMPDIR/
 # Copy all CSV files into directory
 cp ./*.csv $TEMPDIR/
 
+echo "$VARNAMES"
 # Create a new csv with variables in a header row
-echo "$VARNAMES" >> $TEMPDIR/00varheader.csv
+echo "$VARNAMES" > $TEMPDIR/00varheader.csv
 # concatenate all data
-cat $TEMPDIR/*.csv >> ./compiled-data.csv
+cat $TEMPDIR/*.csv > ./compiled-data.csv
 
 # clean up
 rm -r $TEMPDIR
