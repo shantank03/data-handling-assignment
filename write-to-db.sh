b63771e0201662f8560df6a97045e757cb853ce5
shantank <shantank@live.unc.edu>
2017-07-24 22:51:45 -0400

    edit

--- write-to-db.sh
+++ write-to-db.sh
@@ -11,7 +11,7 @@ cp ./*.csv $TEMPDIR/
 
 echo "$VARNAMES"
 # Create a new csv with variables in a header row
-echo "$VARNAMES" > $TEMPDIR/00varheader.csv
+paste "$VARNAMES" $TEMPDIR/00varheader.csv
 # concatenate all data
 cat $TEMPDIR/*.csv > ./compiled-data.csv
 
